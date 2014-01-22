ad656b17debdeeb46aa7f8f853ffdb102e0b18ef
Christos Louizos <chr.louizos@gmail.com>
2014-01-22 12:58:08 +0100

    Added comments at the DPC.

--- ProjectAI/clustering/DPC.java
+++ ProjectAI/clustering/DPC.java
@@ -3,11 +3,13 @@ package clustering;
 import gov.sandia.cognition.learning.algorithm.clustering.DirichletProcessClustering;
 import gov.sandia.cognition.learning.algorithm.clustering.cluster.GaussianCluster;
 import gov.sandia.cognition.math.matrix.Vector;
+import gov.sandia.cognition.math.matrix.VectorEntry;
 import gov.sandia.cognition.math.matrix.VectorFactory;
 import gov.sandia.cognition.statistics.bayesian.DirichletProcessMixtureModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
 
@@ -20,17 +22,42 @@ public class DPC extends Clustering{
 	public ArrayList<Document> documentObjects = new ArrayList<Document>();
 	String extFilePath;
 	String language;
-	int numComponents;
+	int numInitClusters;
 	int numTopics;
 	int seed;
+	int numIterPerSample;
+	Random random;
+	double initAlpha;
+	boolean recalcAlpha;
+	int burnIn;
 	
-	public DPC(int numComponents, String filePath, String language, int numTopics, int seed, boolean bilingual, String extFilePath) {
-		this.numComponents = numComponents;
+	/**
+	 * Dirichlet Process Mixture clustering, with the Chinese Restaurant Process as the prior on clustering.
+	 * 
+	 * @param numInitClusters - initial estimate of the clusters (can change)
+	 * @param filePath - filePath that will be used for the evaluation
+	 * @param language - language of the documents being clustered
+	 * @param numIterPerSample - How many iterations the Gibbs sampler will perform for each data point
+	 * @param initAlpha - initial alpha, parameter of the Dirichlet distribution
+	 * @param recalcAlpha - Define if you want the alpha to adapt, or stay fixed
+	 * @param burnIn - Number of iterations before the sampler starts saving the results
+	 * @param numTopics - number of dimensions at the feature vectors (needed in order to convert to the format the library needs)
+	 * @param seed - seed of the random number generator
+	 * @param bilingual - if bilingual documents are being used
+	 * @param extFilePath - the file path of the external dataset
+	 */
+	
+	public DPC(int numInitClusters, String filePath, String language, int numIterPerSample, double initAlpha, boolean recalcAlpha, int burnIn, int numTopics, int seed, boolean bilingual, String extFilePath) {
+		this.numInitClusters = numInitClusters;
 		this.numTopics = numTopics;
 		this.seed = seed;
 		this.filePath = filePath;
 		this.extFilePath = extFilePath;
-		
+		this.numIterPerSample = numIterPerSample;
+		random = new Random(seed);
+		this.initAlpha = initAlpha;
+		this.recalcAlpha = recalcAlpha;
+		this.burnIn = burnIn;
 		if(!bilingual)
 			this.language = language;
 		else
@@ -44,12 +71,12 @@ public class DPC extends Clustering{
 	}
 	
 	public void startClustering(){
-		
+		// parse the external dataset
 		init_external();
-		Random random = new Random(seed);
-		
+				
 		ArrayList<Vector> data = new ArrayList<Vector>();
 		
+		// perform the connection between filenames and Vector representations
 		Map<String, String> inv_mapping = new HashMap<String,String>();
 		
 		for (Document doc : documentObjects){
@@ -68,20 +95,15 @@ public class DPC extends Clustering{
 		DirichletProcessClustering dpc = new DirichletProcessClustering();
 		DirichletProcessMixtureModel<Vector> alg = dpc.getAlgorithm();
 		
-		int numInitClusters = 10;
-		int numIterations = 10;
-		// set this to false for best results at monolingual
-		boolean recalcAlpha = false;
+		// recalcAlpha false for best results at monolingual
 		// best alpha for monolingual is 100 (too much probably, why?)
-		double initAlpha = 100;
-		int burnIn = 2;
 		
 		System.out.println("Initial Parameters:");
-		System.out.println("numInitClusters: "+numInitClusters+", numIterations: "+numIterations+", recalcAlpha: "+recalcAlpha+", initAlpha: "+initAlpha+", burnIn: "+burnIn);
+		System.out.println("numInitClusters: "+numInitClusters+", numIterations: "+numIterPerSample+", recalcAlpha: "+recalcAlpha+", initAlpha: "+initAlpha+", burnIn: "+burnIn);
 		System.out.println();
 		
 		alg.setNumInitialClusters(numInitClusters);
-		alg.setIterationsPerSample(numIterations);
+		alg.setIterationsPerSample(numIterPerSample);
 		alg.setRandom(random);
 		alg.setReestimateAlpha(recalcAlpha);
 		alg.setInitialAlpha(initAlpha);
@@ -99,6 +121,19 @@ public class DPC extends Clustering{
         	Centroid dummycent = new Centroid();
         	Cluster cluster = new Cluster(dummycent);
         	
+        	// set the mean of the Gaussian as the cluster centroid
+        	Vector mean = res.get(i).getGaussian().getMean();
+        	Map<String, Double> cent_dist = new HashMap<String, Double>();
+          	Iterator<VectorEntry> it = mean.iterator();
+          	int j = 0;
+          	while(it.hasNext()){
+          		cent_dist.put(Integer.toString(j), it.next().getValue());
+          		j++;
+          	}
+          	cluster.centroid.distribution = cent_dist;
+        	
+          	
+          	// add the members at the clusters
         	ArrayList<Vector> members = res.get(i).getMembers();
         	for(Vector member : members){
         		String filename = inv_mapping.get(member.toString());
