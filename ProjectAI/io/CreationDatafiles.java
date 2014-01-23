38c6a0dd0e7ea7f54f60cee9cc4554335c0fef3b
Duy Chuan <duy.chuan@gmail.com>
2014-01-23 08:39:28 +0100

    !! Remove the content of folders Dutch and English under Testdata (the GB ones), they are inconsistent. I'll put the bigger set on dropbox. Changed Class to make it easier to create a dataset for a certain size, input and output.

--- ProjectAI/io/CreationDatafiles.java
+++ ProjectAI/io/CreationDatafiles.java
@@ -21,12 +21,17 @@ public class CreationDatafiles{
 		boolean createDummyfiles = true; 
 		
 		if(createDummyfiles){
+			String language = "English";
+			//String language = "Dutch";
+			String output_dir = "../Testdata/";
+			output_dir = output_dir + language + "/";
+			
 			// Directory that contains the files of which dummyfiles should
 			// be created.
-			String directory = "../NEWDATA/DUTCH/FIRST"; 
+			String directory = "../Testdata/dummy/"+language+"/"; 
 			String prefix = "D_"; // Desired prefix of the resulting dummy files
 			ArrayList<String> files = FileLoadingUtils.listFilesDirectory(directory);
-			String dummy = "dummy"; // dummyword to which a number will be concatenated
+			String dummy = "dop"; // dummyword to which a number will be concatenated
 			ArrayList<String> dummywords = new ArrayList<String>();
 			for( int d = 1; d <= files.size(); d++ ){
 				String dummyword = dummy+d+dummy;
@@ -42,7 +47,7 @@ public class CreationDatafiles{
 				try{
 					BufferedReader br = new BufferedReader(new FileReader(file));
 					String line;
-					PrintWriter writer = new PrintWriter(file2);
+					PrintWriter writer = new PrintWriter(output_dir+file2);
 					int count5 = 1;
 					while((line = br.readLine()) != null) {
 	            		Scanner s = new Scanner(line);
@@ -69,16 +74,30 @@ public class CreationDatafiles{
 		else{
 			// With the following code one can create snippets from files with 
 			// a certain number of lines
-			String directory = "../TAUS/uk/nl"; // UK-dutch directory 
-			String prefix = "CK"; // change to CS for US-english/dutch dataset
-			String extension = ".nl"; // Dutch
+			String language = "English";
+			//String language = "Dutch";
+			
+			//String directory = "../data/TAUS/uk/nl"; // UK-dutch directory
+			//String directory = "../data/TAUS/us/nl"; // US-dutch directory
+			//String directory = "../data/TAUS/uk/en"; // UK-english directory
+			String directory = "../data/TAUS/us/en"; // US-english directory
+			
+			String output_dir = "../Testdata/";
+			output_dir = output_dir + language + "/";
+			
+			String prefix = "CS"; // CS for US-english/dutch dataset, CK for UK-english/dutch
+			
+			//String extension = ".nl"; // Dutch
+			String extension = ".en"; // English
+			
 			ArrayList<String> files = FileLoadingUtils.listFilesDirectory(directory);
 			int desiredNumberOfLines = 2000;
+			int fileMax = 40;
 	
 				for( int i = 0; i < files.size(); i++ ){
 					if( files.get(i).contains(extension) ){
 					String file = files.get(i);
-					int index = file.indexOf(".nl"); // change to .en for english
+					int index = file.indexOf(".en"); // change to .en for english
 					int index2 = file.lastIndexOf("/")+1;
 					int fileN = 1;
 					String file2 = prefix + file.substring(index2, index) + 
@@ -88,9 +107,9 @@ public class CreationDatafiles{
 					try{
 						BufferedReader br = new BufferedReader(new FileReader(file));
 						String line;
-						PrintWriter writer2 = new PrintWriter(file2);
+						PrintWriter writer2 = new PrintWriter(output_dir+file2);
 		
-						while((line = br.readLine()) != null) {
+						while((line = br.readLine()) != null && fileN<=fileMax) {
 							if( counter <= desiredNumberOfLines ){
 								writer2.println(line);
 							}
@@ -99,7 +118,7 @@ public class CreationDatafiles{
 								fileN++;
 								file2 = prefix + file.substring(index2, index) + 
 										fileN + extension;
-								writer2 = new PrintWriter(file2);
+								writer2 = new PrintWriter(output_dir+file2);
 								writer2.println(line);
 								counter = 1;
 							}
