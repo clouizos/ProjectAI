41be4eefca8504256d8cd98a1061bc91facc1b2c
saidalfaraby <said.al.faraby@gmail.com>
2014-01-26 19:19:01 +0100

    update entropy

--- ProjectAI/src/main/java/com/ICSMT/cluster_evaluation/Entropy.java
+++ ProjectAI/src/main/java/com/ICSMT/cluster_evaluation/Entropy.java
@@ -3,9 +3,9 @@ package cluster_evaluation;
 import java.util.HashMap;
 
 import clustering.Clustering;
+import io.IOFile;
 
 public class Entropy extends ExtrinsicEvaluation{
-	
 	/**
 	 * @param args
 	 */
@@ -14,9 +14,16 @@ public class Entropy extends ExtrinsicEvaluation{
 	public void computeScore(Clustering C) {
 		HashMap<String, HashMap<String, Integer>> confMatrix = createConfusionMatrix(C.filePath, C.clusters);
 		compute(confMatrix);
+		IOFile io = new IOFile();
+		io.openWriteFile("Entropy.csv");
+		io.write(C.ID);
+		io.write(",");
+		io.write(Double.toString(this.score));
+		io.write("\n");
+		io.close();
 	}
 	
-	public static void compute(HashMap<String, HashMap<String, Integer>>confMatrix){
+	public void compute(HashMap<String, HashMap<String, Integer>>confMatrix){
 		
 		if (confMatrix.size()==0){return;}
 		
@@ -25,18 +32,24 @@ public class Entropy extends ExtrinsicEvaluation{
 		for (String cluster : confMatrix.keySet()){
 			if (cluster=="total"){ continue; }
 			double entropy=0;
+			
 			for (String label : confMatrix.get(cluster).keySet()){
 				if (label=="total"){ continue; }
 				double tp = confMatrix.get(cluster).get(label);
 				double totalInCluster = confMatrix.get(cluster).get("total");
-				double precision = Math.max(Double.MIN_VALUE, tp/totalInCluster);
-				entropy+= -precision*Math.log(precision);
+				double precision = tp/totalInCluster;
+				if (precision>0)
+					entropy += -(precision*Math.log(precision));
 			}
+			
 			System.out.println("Evaluation on Cluster "+cluster);
 			System.out.println("Entropy : "+entropy);
 			totalEntropy+=(confMatrix.get(cluster).get("total")/totalDocs)*entropy;
 		}
 		System.out.println("Entropy total : "+totalEntropy);
+		this.score = totalEntropy;
+		
+
 		
 	}
 	
