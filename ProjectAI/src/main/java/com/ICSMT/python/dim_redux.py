91fe8365c4463f03eec38337b2a1d377f6a1eabd
Duy Chuan <duy.chuan@gmail.com>
2014-01-28 13:47:35 +0100

    dim_redux finalized, bunch of featurematrices, non negative featurematrix output.

--- ProjectAI/src/main/java/com/ICSMT/python/dim_redux.py
+++ ProjectAI/src/main/java/com/ICSMT/python/dim_redux.py
@@ -39,32 +39,37 @@ logging.basicConfig(level=logging.INFO,
 #op = OptionParser()
 op = argparse.ArgumentParser(description='Dimensionality reduction.')
 op.add_argument('-l', choices=['English', 'Dutch', 'Both'], nargs='?',
-              help="State which language to perform dim. reduction on.")
+                help="State which language to perform dim. reduction on.")
 op.add_argument('-d', choices='2345', default='',
-                help='dataset help')
-op.add_argument('method', choices=['lsa', 'cca', 'lda2lsa'], nargs='?',
+                help='Which dataset to operate on.')
+op.add_argument('method', choices=['lsa', 'cca', '2monolsa', 'lsa2cca',
+                'lda2lsa', 'lda2cca', 'bilingual2lsa'], nargs='?',
                 help="State what method to run.")
-op.add_argument('-c', type=int, nargs='?', help='components help', default=100)
+op.add_argument('-c', type=int, nargs='?', help='Numbers of components of \
+                the featurematrix.', default=100)
 op.add_argument('-direction', choices=['e2f', 'f2e'], nargs='?',
                 help="Which direction to perform cca on.")
 op.add_argument('-i', nargs='+', type=argparse.FileType('r'),
-                    default=sys.stdin, help='input files for ' + \
-                    'lda and bilangual representation methods.')                             
+                default=sys.stdin, help='Input files for ' + \
+                'lda and bilangual representation methods.')
+op.add_argument("-nn",
+                action="store_true", default=False,
+                help="Enable non-negative feature output.")                             
 op.add_argument("--no-idf",
-              action="store_false", dest="use_idf", default=True,
-              help="Disable Inverse Document Frequency feature weighting.")
+                action="store_false", dest="use_idf", default=True,
+                help="Disable Inverse Document Frequency feature weighting.")
 op.add_argument("--use-hashing",
-              action="store_true", default=False,
-              help="Use a hashing feature vectorizer")
+                action="store_true", default=False,
+                help="Use a hashing feature vectorizer")
 op.add_argument("--n-features", type=int, default=None,
-              help="Maximum number of features (dimensions)"
+                help="Maximum number of features (dimensions)"
                    "to extract from text.")
-op.add_argument("--no-minibatch",
-              action="store_false", dest="minibatch", default=True,
-              help="Use ordinary k-means algorithm (in batch mode).")
-op.add_argument("--verbose",
-              action="store_true", dest="verbose", default=False,
-              help="Print progress reports inside k-means algorithm.")
+#op.add_argument("--no-minibatch",
+#               action="store_false", dest="minibatch", default=True,
+#               help="Use ordinary k-means algorithm (in batch mode).")
+#op.add_argument("--verbose",
+#               action="store_true", dest="verbose", default=False,
+#               help="Print progress reports inside k-means algorithm.")
 
 op.print_help()
 
@@ -186,9 +191,16 @@ method=args.method
 components=args.c
 direction=args.direction
 infiles=args.i
+non_negative=args.nn
+if(non_negative):
+    nn = '_nn'
+else:
+    nn = ''
 print("Language: %s" % language)
 print("Dataset: %s" % dataset)
+print("Method: %s" % method)
 print("Components: %d" % components)
+print("Non-negative: %s" % non_negative)
 
 #language = 'English'
 #language = 'Dutch'
@@ -205,15 +217,20 @@ if method != None:
         # Vectorizer results are normalized, which makes KMeans behave as
         # spherical k-means for better results. Since LSA/SVD results are
         # not normalized, we have to redo the normalization.
-        X = Normalizer(copy=False).fit_transform(X)    
+        if(non_negative):        
+            X = X - X.min()        
+        X = Normalizer(copy=False).fit_transform(X)
+        #print(X.min())
         print("done in %fs" % (time() - t0))
         print()
         output_file = '../features/'+method+'_'+str(components)+'_'+ \
-                    language+dataset+'.data'
+                    language+dataset+nn+'.data'
         output(filenames, X, output_file)
     
     if method == 'cca':
-        cca = CCA(components)
+        if direction == None:
+            print("State direction to perform CCA on!")            
+            raise SystemExit        
         # English set
         docs, path, s_words, filenames = return_Path('English', dataset)
         X = vectorize(path, docs, args.n_features, 
@@ -231,10 +248,13 @@ if method != None:
         else:
             X_ = X
             Y_ = Y
+        cca = CCA(components)    
         cca.fit(X_, Y_)
         CCA(copy=True, max_iter=1000, n_components=components,
             scale=True, tol=1e-06)
         X, Y = cca.transform(X_, Y_)
+        if(non_negative):        
+            X = X - X.min()
         # Vectorizer results are normalized, which makes KMeans behave as
         # spherical k-means for better results. Since LSA/SVD results are
         # not normalized, we have to redo the normalization.
@@ -243,7 +263,7 @@ if method != None:
         print("done in %fs" % (time() - t0))
         print()
         output_file = '../features/'+method+'_'+str(components)+'_'+ \
-                    direction++'_d'+dataset+'.data'
+                    direction+'_d'+dataset+nn+'.data'
         output(filenames, X, output_file)
         
     if method == '2monolsa':
@@ -255,55 +275,112 @@ if method != None:
         docs, path, s_words, filenames2 = return_Path('Dutch', dataset)
         Y = vectorize(path, docs, args.n_features, 
                       s_words, args.use_idf, args.use_hashing)              
-        print("Performing dimensionality reduction using concatenated monolingual lsa")
+        print("Performing dimensionality reduction using parallel \
+        monolingual LSA")
         lsa = TruncatedSVD(components)
         lsa2 = TruncatedSVD(components)
         X = lsa.fit_transform(X)
+        if(non_negative):        
+            X = X - X.min()
         X = Normalizer(copy=False).fit_transform(X)
         Y = lsa2.fit_transform(Y)
+        if(non_negative):        
+            Y = Y - Y.min()
         Y = Normalizer(copy=False).fit_transform(Y)
         print("done in %fs" % (time() - t0))
         print()
-        output_file = '../features/'+method+'_'++str(components)+'_d'+dataset+'.data'
+        output_file = '../features/'+method+'_'+\
+            str(components)+'_d'+dataset+nn+'.data'
         output2(filenames, filenames2, X, Y, output_file)
         
-    if method == 'lda2lsa':
-        if len(infiles) != 2:
-            print("lda2lsa expects 2 corresponding files!")            
-            raise SystemExit
-        print("Performing dimensionality reduction using LSA on LDA")
-        labels1, X = import_lda(infiles[0])
-        labels2, Y = import_lda(infiles[1])
+    if method == 'lsa2cca':
+        if direction == None:
+            print("State direction to perform CCA on!")            
+            raise SystemExit        
+        # English set
+        docs, path, s_words, filenames = return_Path('English', dataset)
+        X = vectorize(path, docs, args.n_features, 
+                      s_words, args.use_idf, args.use_hashing)
+        # Dutch set
+        docs, path, s_words, filenames2 = return_Path('Dutch', dataset)
+        Y = vectorize(path, docs, args.n_features, 
+                      s_words, args.use_idf, args.use_hashing)              
+        print("Performing dimensionality reduction using CCA after \
+        parallel monolingual LSA")
         lsa = TruncatedSVD(components)
         lsa2 = TruncatedSVD(components)
         X = lsa.fit_transform(X)
+        if(non_negative):        
+            X = X - X.min()
         X = Normalizer(copy=False).fit_transform(X)
         Y = lsa2.fit_transform(Y)
+        if(non_negative):        
+            Y = Y - Y.min()
         Y = Normalizer(copy=False).fit_transform(Y)
-        print("done in %fs" % (time() - t0))
-        print()
-        output_file = '../features/'+method+'_'+str(components)+'.data'
-        output2(labels1, labels2, X, Y, output_file)
-        
-    if method == 'lda2cca':
-        if len(infiles) != 2:
-            print("lda2cca expects 2 corresponding files!")            
-            raise SystemExit
-        print("Performing dimensionality reduction using CCA on LDA")
-        labels1, X = import_lda(infiles[0])
-        labels2, Y = import_lda(infiles[1])    
-        X = X.toarray()
-        Y = Y.toarray()
         if direction == 'f2e':
             X_ = Y
             Y_ = X
         else:
             X_ = X
             Y_ = Y
+        cca = CCA(components)
         cca.fit(X_, Y_)
         CCA(copy=True, max_iter=1000, n_components=components,
             scale=True, tol=1e-06)
         X, Y = cca.transform(X_, Y_)
+        if(non_negative):        
+            X = X - X.min()
+        print("done in %fs" % (time() - t0))
+        print()
+        output_file = '../features/'+method+'_'+str(components)+'_'+ \
+                    direction+'_d'+dataset+nn+'.data'
+        output(filenames, X, output_file)
+        
+    if method == 'lda2lsa':
+        if len(infiles) != 2:
+            print("lda2lsa expects 2 corresponding files!")            
+            raise SystemExit
+        print("Performing dimensionality reduction using LSA on LDA.")
+        labels1, X = import_lda(infiles[0])
+        labels2, Y = import_lda(infiles[1])
+        lsa = TruncatedSVD(components)
+        lsa2 = TruncatedSVD(components)
+        X = lsa.fit_transform(X)
+        if(non_negative):        
+            X = X - X.min()
+        X = Normalizer(copy=False).fit_transform(X)
+        Y = lsa2.fit_transform(Y)
+        if(non_negative):        
+            Y = Y - Y.min()        
+        Y = Normalizer(copy=False).fit_transform(Y)
+        print("done in %fs" % (time() - t0))
+        print()
+        output_file = '../features/'+method+'_'+str(components)+nn+'.data'
+        output2(labels1, labels2, X, Y, output_file)
+        
+    if method == 'lda2cca':
+        if len(infiles) != 2:
+            print("lda2cca expects 2 corresponding files!")            
+            raise SystemExit
+        elif direction == None:
+            print("State direction to perform CCA on!")            
+            raise SystemExit
+        print("Performing dimensionality reduction using CCA on LDA.")
+        labels1, X = import_lda(infiles[0])
+        labels2, Y = import_lda(infiles[1])    
+        if direction == 'f2e':
+            X_ = Y
+            Y_ = X
+        else:
+            X_ = X
+            Y_ = Y
+        cca = CCA(components)    
+        cca.fit(X_, Y_)
+        CCA(copy=True, max_iter=1000, n_components=components,
+            scale=True, tol=1e-06)
+        X, Y = cca.transform(X_, Y_)
+        if(non_negative):        
+            X = X - X.min()        
         # Vectorizer results are normalized, which makes KMeans behave as
         # spherical k-means for better results. Since LSA/SVD results are
         # not normalized, we have to redo the normalization.
@@ -312,8 +389,37 @@ if method != None:
         print("done in %fs" % (time() - t0))
         print()
         output_file = '../features/'+method+'_'+str(components)+'_'+ \
-                    direction+'.data'
+                    direction+nn+'.data'
         output(labels1, X, output_file)
+        
+    if method == 'bilingual2lsa':
+        if len(infiles) != 1:
+            print("Bilingual representation to LSA expects 1 input file!")            
+            raise SystemExit
+        print("Performing dimensionality reduction using LSA on \
+        Bilingual representation.")
+        labels1, X = import_lda(infiles[0])
+        lsa = TruncatedSVD(components)
+        X = lsa.fit_transform(X)
+        if(non_negative):        
+            X = X - X.min()
+        X = Normalizer(copy=False).fit_transform(X)
+        print("done in %fs" % (time() - t0))
+        print()
+        output_file = '../features/'+method+'_'+str(components)+nn+'.data'
+        output(labels1, X, output_file)
+        
+else:     
+    if language == None:
+        print("Please input a dataset!")            
+        raise SystemExit    
+    t0 = time()    
+    print("Vectorizing the dataset.")    
+    docs, path, s_words, filenames = return_Path(language, dataset)
+    X = vectorize(path, docs, args.n_features, 
+                  s_words, args.use_idf, args.use_hashing)
+    print("done in %fs" % (time() - t0))
+    print()
         
 ###############################################################################
 # Do the actual clustering
